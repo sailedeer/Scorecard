614c068d74ed2d3f2496fef6cf233832d7db804f
Eli Reed <sailedeer@live.com>
2017-02-15 15:51:51 -0800

    Started design of main activity layout. Added Material Design support library to dependencies. Included AddPlayersActivity

---
+++ app/src/main/java/sailedeer/scorecard/AddPlayersActivity.java
@@ -0,0 +1,12 @@
+package sailedeer.scorecard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPlayersActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_players);
+    }
+}
